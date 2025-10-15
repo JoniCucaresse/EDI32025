47e627ae0b42b296ec927e6a4777994537187359
Joni <jonatancucaresse@gmail.com>
2025-10-14 21:02:17 -0300

    Falta agregar los controladores de Discografica y Generos

---
+++ VinylStore.WebApi/Mapping/ArtistaMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using VinylStore.Application.Dtos.Artista;
+using VinylStore.Entities;
+
+namespace VinylStore.WebApi.Mapping
+{
+    public class ArtistaMappingProfile: Profile
+    {
+        public ArtistaMappingProfile()
+        {
+            CreateMap<Artista, ArtistaResponseDto>();
+            CreateMap<ArtistaRequestDto, Artista>();
+        }
+    }
+}
