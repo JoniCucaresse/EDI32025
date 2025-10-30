d5de45ee6b45ce28c0adfafd08204291f749c2b3
Joni <jonatancucaresse@gmail.com>
2025-10-30 20:41:27 -0300

    Correcciones realizadas

--- VinylStore.WebApi/Controllers/DiscograficasController.cs
+++ VinylStore.WebApi/Controllers/DiscograficasController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using VinylStore.Application;
-using VinylStore.Application.Dtos.Artista;
 using VinylStore.Application.Dtos.Discografica;
 using VinylStore.Entities;
 
